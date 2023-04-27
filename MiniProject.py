02759bfec60a37bf4fd5f04edecbafb62cf05b22
Sreyash Kunkekar <sreyashkunkekar2889@gmail.com>
2023-04-27 14:30:24 +0530

    Create MiniProject.py

---
+++ MiniProject.py
@@ -0,0 +1,47 @@
+import chatterbot
+import chatterbot_corpus
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+my_bot = ChatBot(name='PyBot', read_only=True,logic_adapters=['chatterbot.logic.MathematicalEvaluation','chatterbot.logic.BestMatch'])
+small_talk = ['hi there!',
+              'hi!',
+              'how do you do?',
+              'how are you?',
+              'i\'m cool.',
+              'fine, you?',
+              'always cool.',
+              'i\'m ok',
+              'glad to hear that.',
+              'i\'m fine',
+              'glad to hear that.', 
+              'i feel awesome',
+              'excellent, glad to hear that.',
+              'not so good',
+              'sorry to hear that.',
+              'what\'s your name?',
+              'i\'m pybot. ask me a math question, please.']
+math_talk_1 = ['pythagorean theorem',
+               'a squared plus b squared equals c squared.']
+math_talk_2 = ['law of cosines',
+               'c**2 = a**2 + b**2 - 2 * a * b * cos(gamma)']
+list_trainer = ListTrainer(my_bot)
+for item in (small_talk, math_talk_1, math_talk_2):
+    list_trainer.train(item)
+
+from chatterbot.trainers import ChatterBotCorpusTrainer
+corpus_trainer = ChatterBotCorpusTrainer(my_bot)
+corpus_trainer.train('chatterbot.corpus.english')
+
+
+
+while True:
+    user_input = input("You: ")
+    if user_input.lower() == 'exit':
+        break
+    else:
+        print("Bot:", my_bot.get_response(user_input))
+
+    
+    
+
+
